5e84275a597363af947c5e183653a251fb96cd62
TharinduDR <rhtdranasinghe@gmail.com>
2023-10-09 11:16:13 +0100

    001: Initial commit

--- seq2seq/__init__.py
+++ seq2seq/__init__.py
@@ -1,2 +1,1 @@
-from simpletransformers.config.model_args import Seq2SeqArgs
-from simpletransformers.seq2seq.seq2seq_model import Seq2SeqModel
+
--- t5/__init__.py
+++ t5/__init__.py
@@ -1,2 +1,1 @@
-from simpletransformers.config.model_args import T5Args
-from simpletransformers.t5.t5_model import T5Model
+
