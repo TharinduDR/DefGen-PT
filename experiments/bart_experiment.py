ff792b4a0b1ade921fc576d377a4bec970d81f10
TharinduDR <rhtdranasinghe@gmail.com>
2023-10-11 20:24:09 +0100

    001: Initial commit

--- experiments/bart_experiment.py
+++ experiments/bart_experiment.py
@@ -9,7 +9,7 @@ from experiments.evaluate import bleu, ter, bertscore, bleurt_score
 from seq2seq.seq2seq_model import Seq2SeqModel
 
 model_name = "facebook/mbart-large-50"
-model_type = "bart"
+model_type = "mbart50"
 
 SEED = 777
 full = pd.read_csv("data/pt.defmod.wholeset.tag.tsv", sep="\t")
