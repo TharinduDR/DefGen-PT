77194eeee07e8309ad952433c44c49fe406d78f5
TharinduDR <rhtdranasinghe@gmail.com>
2023-10-16 21:14:04 +0100

    001: Initial commit

--- experiments/bert_experiment.py
+++ experiments/bert_experiment.py
@@ -54,7 +54,7 @@ model = Seq2SeqModel(
 
 
 train, eval = train_test_split(full_train, test_size=0.2, random_state=SEED)
-model.train_model(train, eval_data=eval)
+model.train_model(train, eval_data=eval, args=model_args)
 
 input_list = test['input_text'].tolist()
 truth_list = test['target_text'].tolist()
